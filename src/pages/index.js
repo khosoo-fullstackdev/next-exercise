2751679768dac81bc59f677bd00e82d63fb9a083
Saaral_Note <khosoo.fsdev@gmail.com>
2024-01-22 11:14:26 +0800

    awdwada

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
+import { useState, useEffect } from "react";
+
 export default function Home() {
-  async function getData() {
-    const response = await fetch("https://randomuser.me/api/");
-    const data = await response.json();
-    console.log(data);
-    console.log(data.results[0].name.first);
-  }
-  getData();
-  return <div>hi</div>;
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://randomuser.me/api/");
+      const data = await response.json();
+      console.log(data);
+      setUserName(data.results[0].name.first);
+    }
+    getData();
+  }, []);
+  return (
+    <div>
+      <p>{userName}</p>
+    </div>
+  );
 }
-const data = {
-  result: {},
-  info: {},
-};
